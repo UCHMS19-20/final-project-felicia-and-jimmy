c871c5688ba552e8f7c815dcc68afdded3be9e05
jimmysong555 <58479463+jimmysong555@users.noreply.github.com>
2020-01-09 10:35:20 -0500

    Update hockeygame.py

--- src/hockeygame.py
+++ src/hockeygame.py
@@ -1,28 +1,49 @@
 import sys
 import pygame
-from paddle import Paddle
-
-# Initialize pygame so it runs in the background and manages things
+import random
 pygame.init()
 
+# Initialize pygame so it runs in the background and manages things
+
 #defining colors
-LIGHTBLUE = (191, 239, 255)
+
 WHITE=(255, 255, 255)
-
-#setting up display
-screen = pygame.display.set_mode( (700, 500) )
-screen.fill(LIGHTBLUE)
+BLACK=(0, 0, 0)
+BLUE=(0, 255, 255)
+screen = pygame.display.set_mode( (600, 600) )
 pygame.display.set_caption("Air Hockey")
+carryOn=True
 
-#create paddles
-paddleA = Paddle(WHITE, 10, 100)
-paddleA.rect.x=20
-paddleA.rect.y=200
+clock = pygame.time.Clock()
+while carryOn:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            carryOn = False
 
-paddleB = Paddle(WHITE, 10, 100)
-paddleB.rect.x=665
-paddleB.rect.y=200
+    #clearing screen and setting up net
+    screen.fill(BLACK)
+    pygame.draw.line(screen, WHITE, [0, 299], [600, 299],5)
 
-screen.display.flip()
+    #creating puck
+    class Puck(pygame.sprite.Sprite):
+        def __init__(self):
+            super().__init__()
+            self.image = pygame.surface([10, 10])
+            self.image.fill(BLUE)
+            self.rect=self.image.get_rect()
+            self.screenheight=pygame.display.get_surface().get_height()
+            self.screenwidth=pygame.display.get_surface().get_width()
+            self.speed = 0
+            self.x = 0
+            self.y = 0
+            self.direction = 0
+            self.width = 10
+            self.height = 10
 
 
+   
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
