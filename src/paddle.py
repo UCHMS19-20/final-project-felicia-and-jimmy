8c0ba2602be43fc8d20733d50c5a4d536e9eb013
feliciarsu <58041217+feliciarsu@users.noreply.github.com>
2020-01-15 10:37:59 -0500

    Update paddle.py

--- src/paddle.py
+++ src/paddle.py
@@ -1,5 +1,24 @@
 #create paddles for use in hockey
 import pygame
-BLACK=(0, 0, 0)
+WHITE=(255, 255, 255)
 
 #create a paddle class from the Sprite class in pygame. Sprites are a base class for different types of obkects in a game.
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image=pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect=self.image.get.rect()
+
+        def moveLeft(self, pixels):
+            self.rect.x-=pixels
+            if self.rect.x<0:
+                self.rect.x=0
+
+        def moveRight(self, pixels):
+            self.rect.x+=pixels
+            if self.rect.x>600:
+                self.rect.x=600
+            
